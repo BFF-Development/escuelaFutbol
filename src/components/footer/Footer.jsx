e87238d3d5395ca4e00a90d188f6cbf115c1e5d8
facuMendez <facundo.mendez@live.com.ar>
2023-01-06 15:00:04 -0300

    continuing footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,13 @@ import "./footer.css"
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="nav_degree_footer"></div>
-
+      <div className="title_footer"> <h2>Name</h2> </div>
+      <ul className="list_Footer">
+        <li>Inicio</li>
+        <li>Quiene Somos</li>
+        <li>Servicios</li>
+        <li>Contacto</li>  
+      </ul>        
 
     </div>
   )
