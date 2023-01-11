52a7195adefcd7ecbbc97821704415df2578207c
facuMendez <facundo.mendez@live.com.ar>
2023-01-11 15:00:56 -0300

    prueba colores

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./about.css"
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/src/ScrollTrigger'
 
 const About = () => {
+
+  useEffect(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const animateAbout = () => {
+
+
+      gsap.from(".about_title",{
+        opacity:0,
+        y: 100,
+          scrollTrigger:{
+            trigger:".about",
+            start:"-400px",
+            end: "0px",
+            scrub: 1
+          }
+      })
+
+ /*      const tl = gsap.timeline({
+        duration:1,
+        scrollTrigger:{
+          trigger:".about",
+          pin:true,
+          scrub:2,
+          end:"+=1000"
+        }
+      }) */
+  
+ /*      tl.to(".about",{
+        background:"black",
+      })
+
+      tl.to(".about_title",{
+        color:"white",
+      }) */
+  
+ 
+    }
+
+    animateAbout()
+
+  return () => {
+    animateAbout()
+
+  }
+
+
+  }, [])
+
+
   return (
     <div className="about">
         <h2 className='about_title'>Quienes somos?</h2>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -9,7 +9,20 @@ const Services = () => {
     gsap.registerPlugin(ScrollTrigger);
 
     let sections = gsap.utils.toArray(".panel");
+
     
+/*     gsap.to(".backPanel1",{
+      background:" linear-gradient(90deg, rgba(255,214,3,1) 0%, rgba(29,63,253,1) 53%, rgba(252,69,69,1) 100%)",
+      duration:1,
+      scrollTrigger:{
+        trigger:".panel",
+        scrub:1
+      }
+    })
+
+ */
+
+
     gsap.to(sections, {
       xPercent: -100 * (sections.length - 1),
       ease: "none",
@@ -21,12 +34,20 @@ const Services = () => {
         end: () => "+=" + document.querySelector(".container_services").offsetWidth
       }
     });
+
+
+ 
+
   },[])
 
   return (
         
       <div className="container_services" >
-          <section className="panel panel1"></section>
+          <section className="panel panel1">
+            <div className="backPanel1">
+
+            </div>
+          </section>
           <section className="panel panel2"></section>
           <section className="panel panel3"></section>
           <section className="panel panel4"></section>
