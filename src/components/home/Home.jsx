0c1a23ae0f78c90fa18a49db5a0d4182785095b3
facuMendez <facundo.mendez@live.com.ar>
2023-01-12 15:39:23 -0300

    horneando la scena

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./home.css"
+import scene from './scene/scene'
 
 export const Home = () => {
+  useEffect(() => {
+    scene()
+
+  /*   return () => {
+      scene()
+    } */
+  },[])
   
   return (
-    <div className="home"></div>
+    <div className="home">
+      <canvas className='ballWebGl'></canvas>
+
+    </div>
   )
 }
