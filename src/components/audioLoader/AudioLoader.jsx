bfff525ad90f3251725ad5853129b1c1b1e95b10
facuMendez <facundo.mendez@live.com.ar>
2023-01-24 13:55:04 -0300

    responsive contact

--- src/components/audioLoader/AudioLoader.jsx
+++ src/components/audioLoader/AudioLoader.jsx
@@ -29,7 +29,11 @@ const AudioLoader = () => {
     },[activeSound])
 
 
+    const [soundOff, setSoundOff] = useState(true)
 
+    const handlerSound = () => {
+      setSoundOff(!soundOff)
+    }
 
 
 
@@ -37,20 +41,37 @@ const AudioLoader = () => {
     <div className="loader" onClick={handlerActiveSound}>
       <div className="loaderBox">
           <div className="circleBlur"></div>
-          <svg className='circle' xmlns="http://www.w3.org/2000/svg" xmlLang="en" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500">
+          <svg className='circle' xmlns="http://www.w3.org/2000/svg" xmlLang="en" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" onClick={handlerSound}>
             <defs>
               <path id="textcircle" d="M250,400
                           a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z" transform="rotate(12,250,250)" />
             </defs>
-            <g className="textcircle">
-              <text className='textCircle' textLength="942">
-                <textPath 
-                          xlinkHref="#textcircle" 
-                          aria-label="SOUND ON" 
-                          textLength="942"> • ARCO • SOUND ON • ARCO • SOUND ON  &nbsp;
-                </textPath>
-              </text>
-            </g>
+            {
+              soundOff ?
+                <g className="textcircle">
+                  <text className='textCircle' textLength="942">
+                    <textPath 
+                              xlinkHref="#textcircle" 
+                              aria-label="SOUND ON" 
+                              textLength="942">  • ARCO • SOUND ON • ARCO • SOUND ON  &nbsp;
+                    </textPath>
+                  </text>
+                </g>
+
+              :
+
+              <g className="textcircle">
+                <text className='textCircle' textLength="942">
+                  <textPath 
+                            xlinkHref="#textcircle" 
+                            aria-label="SOUND ON" 
+                            textLength="942">  • ARCO • SOUND OFF • ARCO • SOUND OFF  &nbsp;
+                  </textPath>
+                </text>
+              </g>
+
+            }
+
           </svg>
 
 
